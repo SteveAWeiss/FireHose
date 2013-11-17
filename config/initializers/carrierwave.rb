0f19d49e72e2c22f3dc6fb447ac9b7a462f2a7ce
Swice <swice@SwirtualBox.(none)>
2013-11-17 00:07:18 -0500

    Fixing a typo

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],                        # required
       :aws_secret_access_key  => ENV['AWS_SECRET_KEY']
       }
-    config.fog_directory  = ENV['AWS BUCKET']                   # required
+    config.fog_directory  = ENV['AWS_BUCKET']                   # required
   
     config.storage = :fog
   else
